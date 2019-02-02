b9eeed6e893c8bea1b5eccf535faad721e9f22d1
Cooper Lightfoot <cooperlightfoot@Coopers-MacBook-Air.local>
2019-02-02 12:40:08 -0700

    Added semi-functional login page

--- gamersonly/src/App.js
+++ gamersonly/src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from './pages/Login'
+import Feed from './pages/Feed'
+import Settings from './pages/Settings'
+import NavBar from './components/NavBar'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const AppRouter = () => (
+  <Router>
+    <div id="background">
+      <NavBar/>
+      <Route path="/" exact component={Login} />
+      <Route path="/feed/" component={Feed} />
+      <Route path="/settings/" component={Settings} />
+    </div>
+  </Router>
+);
 
-export default App;
+export default AppRouter;
---
+++ gamersonly/src/components/CreateForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class CreateForm extends Component {
+
+  render() {
+    return (
+      <div>
+        <div id="custom-form" className="py-3 container">
+          <form>
+            <div className="form-group">
+              <label id="label-color" htmlFor="exampleInputEmail1">Email2 address</label>
+              <input type="email" autoComplete="username email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+              <small id="label-color" className="form-text">We'll never share your email with anyone else.</small>
+            </div>
+            <div className="form-group">
+              <label id="label-color" htmlFor="exampleInputPassword1">2Password</label>
+              <input type="password" autoComplete="current-password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+            </div>
+            <div className="form-group form-check">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
+              <label id="label-color" className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" className="btn btn-primary">2Submit</button>
+          </form>
+        </div>
+        <div className="mt-5">
+          <div id="custom-form">
+            <p>Don't have an account?<a href="/" onClick={(e)=> {this.props.toggleLogin(e)}}>Click Here</a></p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+}
+export default CreateForm;
